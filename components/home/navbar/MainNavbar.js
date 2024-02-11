fd8cd973925529575997ebc9adbe18d76cad5639
Rubayet-billah <rubayetbillah909@gmail.com>
2024-02-11 22:57:59 +0600

    nav fiz

--- components/home/navbar/MainNavbar.js
+++ components/home/navbar/MainNavbar.js
@@ -46,7 +46,7 @@ const MainNavbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-sm dropdown-content mt-3 z-[2] p-2 shadow bg-base-100 rounded-box w-52"
             >
               {linkTags}
             </ul>
