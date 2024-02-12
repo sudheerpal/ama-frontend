10647212063c82065ef120e49d49c646e3977944
Rubayet-billah <rubayetbillah909@gmail.com>
2024-02-12 23:35:24 +0600

    service card fixed

--- components/home/service/ServiceCard.js
+++ components/home/service/ServiceCard.js
@@ -1,17 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import cardImage from "@/assets/serviceCard/image-1.png";
 import icon from "@/assets/icons/Consulting.png";
 import Image from "next/image";
 
 const ServiceCard = () => {
+  const [isHovered, setIsHoverd] = useState(false);
   return (
-    <div className="relative overflow-hidden transition duration-300 bg-gray-100 shadow-lg hover:shadow-xl">
+    <div
+      onMouseEnter={(e) => setIsHoverd(true)}
+      onMouseLeave={(e) => setIsHoverd(false)}
+      className="relative overflow-hidden transition duration-300 bg-gray-100 shadow-lg hover:shadow-xl"
+    >
       {/* Background image */}
       <div
         className="h-64 bg-center bg-cover"
         style={{ backgroundImage: `url(${cardImage.src})` }}
       ></div>
-      <div className="absolute left-0 right-0 h-full px-4 py-2 text-white transition duration-200 opacity-100 bg-opacity-80 bg-primary top-[200px] backdrop-filter hover:-translate-y-[200px]">
+      <div
+        className={`absolute left-0 right-0 h-full px-4 py-2 text-white transition duration-200 opacity-100 bg-opacity-80 bg-primary top-[200px] backdrop-filter ${
+          isHovered && "-translate-y-[200px]"
+        }`}
+      >
         <div className="flex flex-col items-start gap-2">
           <h3 className="flex items-center gap-3 text-xl font-bold">
             <Image className="w-10 h-10" src={icon} alt="icon" />
