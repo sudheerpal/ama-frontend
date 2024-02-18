113ed100a540c31e62b01c62c44724a58e158f02
Rubayet-billah <rubayetbillah909@gmail.com>
2024-02-18 23:56:09 +0600

    nested navbar edited

--- components/home/navbar/LinkTags.js
+++ components/home/navbar/LinkTags.js
@@ -8,24 +8,52 @@ const LinkTags = () => {
   const links = [
     { text: "Home", url: "/home" },
     { text: "About Us", url: "#" },
-    { text: "Services", url: "#" },
+    {
+      text: "Services",
+      url: "#",
+      submenus: [
+        { text: "Web Design", url: "/services/web-design" },
+        { text: "SEO & SMO", url: "/services/seo-smo" },
+      ],
+    },
     { text: "Verticals", url: "#" },
     { text: "Research Insights", url: "#" },
     { text: "Resources", url: "#" },
     { text: "Contact Us", url: "#" },
   ];
+
   return (
     <>
       {links.map((link, index) => (
         <li key={index} className="text-base text-primary font-[500] mx-1">
-          <Link
-            className={`px-2 py-1 ${
-              pathname == link.url ? "text-blue-500" : ""
-            }`}
-            href={link.url}
-          >
-            {link.text}
-          </Link>
+          {link.submenus ? (
+            <details>
+              <summary className="px-2 py-1 hover:text-blue-500">
+                {link.text}
+              </summary>
+              <ul className="z-10 rounded lg:min-w-60">
+                {link.submenus.map((submenu, idx) => (
+                  <li key={idx}>
+                    <Link
+                      href={submenu.url}
+                      className="px-2 py-1 hover:text-blue-500"
+                    >
+                      {submenu.text}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </details>
+          ) : (
+            <Link
+              href={link.url}
+              className={`px-2 py-1 hover:text-blue-500 ${
+                pathname == link.url ? "text-blue-500" : ""
+              }`}
+            >
+              {link.text}
+            </Link>
+          )}
         </li>
       ))}
     </>
