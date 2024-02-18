efb511574bff46b22b498bd07b980e90d8f1cad1
Rubayet-billah <rubayetbillah909@gmail.com>
2024-02-18 22:25:43 +0600

    count animation successfully implemented

--- components/home/stat/Stats.js
+++ components/home/stat/Stats.js
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import CountUp from "react-countup";
 
 const Stats = () => {
   const statsData = [
@@ -8,11 +10,46 @@ const Stats = () => {
     { value: "100%", label: "Satisfied customers" },
   ];
 
+  const [isCounting, setIsCounting] = useState(false);
+  const countRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5, // When 50% of the target is visible in the viewport
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsCounting(true);
+        }
+      });
+    }, options);
+
+    if (countRef.current) {
+      observer.observe(countRef.current);
+    }
+
+    return () => {
+      if (countRef.current) {
+        observer.unobserve(countRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="grid grid-cols-2 mt-12 font-bold text-white lg:grid-cols-4 lg:mt-24">
       {statsData.map((stat, idx) => (
         <div key={idx} className="px-2 py-2 lg:px-6 lg:py-3">
-          <h1 className="text-2xl md:text-4xl lg:text-6xl">{stat.value}</h1>
+          <h1 ref={countRef} className="text-2xl md:text-4xl lg:text-6xl">
+            {isCounting ? (
+              <CountUp end={+stat.value}>{stat.value}</CountUp>
+            ) : (
+              stat.value
+            )}
+          </h1>
           <h3 className="sm:text-lg md:text-xl">{stat.label}</h3>
         </div>
       ))}
