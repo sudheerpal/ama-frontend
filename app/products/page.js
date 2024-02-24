9d68554e4225a48bf40b097caae863d3e0879782
Rubayet-billah <rubayetbillah909@gmail.com>
2024-02-25 00:14:17 +0600

    refcatored

--- app/products/page.js
+++ app/products/page.js
@@ -14,11 +14,7 @@ const Products = () => {
             <ProductsList />
           </div>
           <div className="sticky top-0 hidden lg:block">
-            {/* <ProductSidebar /> */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus id at
-            quo molestiae labore dolorem expedita, commodi amet tempore eum unde
-            tempora, repudiandae harum obcaecati fugiat assumenda distinctio
-            quibusdam iure!lorem40
+            <ProductSidebar />
           </div>
         </section>
       </CustomContainer>
