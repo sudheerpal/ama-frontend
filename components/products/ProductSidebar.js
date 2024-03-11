1459a93b14376004a2e44e1dc87bb90d82b1ca68
Rubayet-billah <rubayetbillah909@gmail.com>
2024-03-11 23:11:47 +0600

    nav element fixed

--- components/products/ProductSidebar.js
+++ components/products/ProductSidebar.js
@@ -1,45 +1,12 @@
-"use client";
-import Link from "next/link";
-import { useState } from "react";
-import { Minus, Plus } from "react-feather";
+import NavElement from "./components/NavElement";
 
 const ProductSidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="mt-5">
       <h3 className="mb-5 text-xl font-bold text-primary">Indutry Experties</h3>
       <div>
         {"123456789".split("").map((el) => (
-          <div
-            key={el}
-            className="p-0 border rounded collapse hover:bg-primary hover:text-white"
-          >
-            <input
-              className="p-0"
-              onClick={() => setIsOpen(!isOpen)}
-              type="checkbox"
-            />
-            <div className="flex items-center justify-between px-4 py-1 font-medium collapse-title">
-              <p>HealthCare</p>
-              <span>{isOpen ? <Minus /> : <Plus />}</span>
-            </div>
-            <div className="collapse-content">
-              <ul className="mt-2 space-y-2">
-                <li className="text-sm text-gray-400 hover:text-white">
-                  <Link href="">Medical device</Link>
-                </li>
-                <li className="text-sm text-gray-400 hover:text-white">
-                  <Link href="">Pharmaceutical</Link>
-                </li>
-                <li className="text-sm text-gray-400 hover:text-white">
-                  <Link href="">Healthcare IT</Link>
-                </li>
-                <li className="text-sm text-gray-400 hover:text-white">
-                  <Link href="">Pipeline Review</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <NavElement key={el} />
         ))}
       </div>
     </div>
