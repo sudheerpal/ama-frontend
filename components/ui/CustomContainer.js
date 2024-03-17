fb7b8c2a5139382cc29ecd4f2b11e0fed150fd52
Rubayet-billah <rubayetbillah909@gmail.com>
2024-03-17 21:46:40 +0600

    issue fixed

--- components/ui/CustomContainer.js
+++ components/ui/CustomContainer.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const CustomContainer = ({ children, classNames = "" }) => {
   return (
-    <div className={"w-full max-w-[1440px] mx-auto px-5 lg:px-10" + classNames}>
+    <div
+      className={
+        "w-full max-w-[1440px] mx-auto px-5 lg:px-10" + " " + classNames
+      }
+    >
       {children}
     </div>
   );
