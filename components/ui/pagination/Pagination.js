e918e37ac8493b00209fef89df6b20f758c3aa5e
Rubayet-billah <rubayetbillah909@gmail.com>
2024-03-20 16:50:24 +0600

    pagination refactored

--- components/ui/pagination/Pagination.js
+++ components/ui/pagination/Pagination.js
@@ -4,7 +4,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   // Function to generate an array of page numbers around the current page
   const generatePageNumbers = () => {
     const pages = [];
-    const maxPagesToShow = 5; // Maximum number of pages to show
+    const maxPagesToShow = 3; // Maximum number of pages to show
 
     // Calculate the range of page numbers to display
     let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
@@ -15,16 +15,30 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       startPage = Math.max(1, endPage - maxPagesToShow + 1);
     }
 
+    // Special case handling for very first pages
+    if (startPage > 2) {
+      pages.push(1, 2, "...");
+    } else if (startPage === 2) {
+      pages.push(1, 2);
+    }
+
     // Generate the array of page numbers
     for (let i = startPage; i <= endPage; i++) {
       pages.push(i);
+    }
+
+    // Special case handling for very last pages
+    if (totalPages - endPage > 1) {
+      pages.push("...", totalPages - 1, totalPages);
+    } else if (totalPages - endPage === 1) {
+      pages.push(totalPages - 1, totalPages);
     }
 
     return pages;
   };
 
   return (
-    <div className="my-12">
+    <div className="my-12 lg:items-center lg:flex">
       <div className="block mx-auto rounded join w-fit">
         {/* Previous page button */}
         <button
@@ -36,16 +50,27 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         </button>
 
         {/* Page buttons */}
-        {generatePageNumbers().map((pageNumber) => (
-          <button
-            key={pageNumber}
-            className={`join-item btn btn-sm md:w-12 ${
-              pageNumber === currentPage ? "btn-primary" : ""
-            }`}
-            onClick={() => onPageChange(pageNumber)}
-          >
-            {pageNumber}
-          </button>
+        {generatePageNumbers().map((pageNumber, index) => (
+          <React.Fragment key={index}>
+            {pageNumber === "..." ? (
+              <button className="join-item btn btn-sm" disabled>
+                {pageNumber}
+              </button>
+            ) : (
+              <button
+                className={`join-item btn btn-sm md:w-12 ${
+                  pageNumber === currentPage ? "btn-primary" : ""
+                }`}
+                onClick={() =>
+                  typeof pageNumber === "number"
+                    ? onPageChange(pageNumber)
+                    : null
+                }
+              >
+                {pageNumber}
+              </button>
+            )}
+          </React.Fragment>
         ))}
 
         {/* Next page button */}
@@ -56,6 +81,22 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         >
           {">>"}
         </button>
+      </div>
+      <div className="flex gap-5 mx-auto my-2 w-fit">
+        <input
+          type="number"
+          placeholder="Jump to Page"
+          className="w-full rounded input input-bordered input-sm focus:outline-none"
+          onChange={(e) => onPageChange(+e.target.value)}
+        />
+        <select className="w-full rounded select select-bordered select-sm focus:outline-none">
+          <option disabled selected>
+            Reports Per Page
+          </option>
+          <option>5</option>
+          <option>10</option>
+          <option>20</option>
+        </select>
       </div>
     </div>
   );
