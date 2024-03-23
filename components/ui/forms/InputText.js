715e3966c4a07b966db097cdf274d5c0844876c3
Rubayet-billah <rubayetbillah909@gmail.com>
2024-03-24 01:22:06 +0600

    error solved

--- components/ui/forms/InputText.js
+++ components/ui/forms/InputText.js
@@ -3,16 +3,14 @@ import React from "react";
 
 const InputText = ({ classNames, placeholder, name, value, onChange }) => {
   return (
-    <div className={`text-sm ${classNames}`}>
-      <input
-        className={`w-full px-4 py-3 bg-gray-200 focus:outline-none`}
-        type="text"
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
+    <input
+      className={`w-full px-4 py-3 bg-gray-200 focus:outline-none text-sm ${classNames}`}
+      type="text"
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
   );
 };
 
