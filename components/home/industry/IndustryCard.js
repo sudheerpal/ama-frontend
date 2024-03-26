6c8cd2409ad06b7f7c049ac941f8430486919542
Rubayet-billah <rubayetbillah909@gmail.com>
2024-03-27 01:07:31 +0600

    style refactored in the card

--- components/home/industry/IndustryCard.js
+++ components/home/industry/IndustryCard.js
@@ -11,20 +11,20 @@ const IndustryCard = ({ industry }) => {
     <div
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      className="flex items-center gap-2 px-2 py-12 duration-300 hover:bg-white"
+      className="flex items-center gap-2 px-2 py-5 duration-300 hover:bg-white min-h-60"
     >
       <div>
         <div
-          className={`w-0 h-8 mx-auto border-r-8 border-secondary ${
-            isHovered ? "visible" : "invisible translate-y-16"
+          className={`w-0 mx-auto border-r-8 border-secondary ${
+            isHovered ? "visible h-8" : "invisible h-0"
           } duration-300`}
         ></div>
         <div className="relative z-10 w-20">
-          <Image className="w-full" src={icon} alt={name} />
+          <Image className={`w-full`} src={icon} alt={name} />
         </div>
         <div
-          className={`w-0 h-8 mx-auto border-r-8 border-secondary ${
-            isHovered ? "visible" : "invisible -translate-y-16"
+          className={`w-0 mx-auto border-r-8 border-secondary ${
+            isHovered ? "visible h-8" : "invisible h-0"
           } duration-300`}
         ></div>
       </div>
