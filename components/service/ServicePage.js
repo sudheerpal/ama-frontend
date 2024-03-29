204e62ada80facfdc00cb9f002f67cc503756970
Rubayet-billah <rubayetbillah909@gmail.com>
2024-03-30 01:02:17 +0600

    not found page designed

--- components/service/ServicePage.js
+++ components/service/ServicePage.js
@@ -3,7 +3,11 @@ import React from "react";
 const ServicePage = () => {
   return (
     <div>
-      <section></section>
+      <section>
+        <h1 className="mt-8 text-3xl font-bold text-center">
+          Service page is under construction
+        </h1>
+      </section>
     </div>
   );
 };
