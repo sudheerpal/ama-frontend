d04e2647ab375f9e60b0f18955534c133eedff6b
basir5101 <basir.bsmrstu@gmail.com>
2024-03-30 00:11:56 +0600

    rd page work

--- components/report/components/SummaryTabContent.js
+++ components/report/components/SummaryTabContent.js
@@ -1,141 +1,162 @@
+"use client";
 /* eslint-disable react/no-unescaped-entities */
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import barChart from "@/assets/report/barChart.png";
 import roundChart from "@/assets/report/roundChart.png";
 import Image from "next/image";
 import SummaryTabFAQ from "./SummaryTabFAQ";
 import SummaryTabHighlight from "./SummaryTabHighlight";
+import RegionData from "./RegionData";
 
-const SummaryTabContent = () => {
+const SummaryTabContent = ({ basic, marketAnalysis, rd, marketReport }) => {
+  const [Chart, setChart] = useState(null);
+  useEffect(() => {
+    import("react-apexcharts").then((mod) => {
+      setChart(() => mod.default);
+    });
+  }, []);
+
+  const growthChart = useRef(null);
+  const sizeChart = useRef(null);
+  const regionalChart = useRef(null);
+  const rdContent = useRef(null);
+  const growthRef = useRef(null);
+  const sizeRef = useRef(null);
+  let chartData = {};
+  try {
+    chartData = JSON.parse(rd.chart);
+  } catch (error) {
+    // console.error(error)
+  }
+  let newData = {};
+  if (chartData && chartData.regional_market_share) {
+    if (typeof Object.values(chartData.regional_market_share)[0] === "object") {
+      for (const key in chartData.regional_market_share) {
+        const firstYearValue = chartData.regional_market_share[key]["2023"];
+        newData[key] = firstYearValue;
+      }
+    } else newData = chartData.regional_market_share;
+  }
+
+  useEffect(() => {
+    if (rdContent.current && growthRef.current) {
+      try {
+        const h3Elements = Array.from(rdContent.current.querySelectorAll("h3"));
+        console.log("h3 elements", h3Elements);
+        h3Elements.forEach((h3) => {
+          if (h3.textContent.includes("Growth Catalysts")) {
+            h3.parentNode.insertBefore(growthRef.current, h3.nextSibling);
+          }
+        });
+      } catch (error) {
+        console.error("error", error);
+      }
+    }
+    if (rdContent.current && sizeRef.current) {
+      try {
+        const h3Elements = Array.from(rdContent.current.querySelectorAll("h3"));
+        console.log("h3 elements", h3Elements);
+        h3Elements.forEach((h3) => {
+          if (h3.textContent.includes("Driving Forces")) {
+            h3.parentNode.insertBefore(sizeRef.current, h3.nextSibling);
+          }
+        });
+      } catch (error) {
+        console.error("error", error);
+      }
+    }
+  }, [rdContent, growthRef, chartData]);
+
   return (
     <div>
-      <section className="mb-8">
-        <header className="my-4">
-          <h3 className="text-xl font-bold uppercase border-b-4 text-primary border-secondary w-fit">
-            Key Market Insights
-          </h3>
-        </header>
-        <main>
-          <p>
-            Key market insights provide invaluable understanding and foresight
-            into the dynamics shaping various industries, serving as a compass
-            for businesses navigating turbulent waters. These insights encompass
-            a multitude of factors, including consumer behavior, emerging
-            technologies regulatory landscapes, and competitive forces. Delving
-            into consumer behavior unveils shifting preferences, buying
-            patterns, and demographic trends, essential for crafting targeted
-            marketing strategies and product development. Understanding emerging
-            technologies allows companies to embrace innovation streamline
-            processes, and stay ahead of the curve in an increasingly digital
-            world. Moreover, keeping a pulse on regulatory landscapes ensures
-            compliance and mitigates risks asociated with legal challe-nges or
-            reputational damage. Concurrently, analyzing competitive forces
-            unveils market gaps, threats, and opportunities, empowering
-            businesses to fine-tune their positioning and capitalize on untapped
-            potential. In essence, key market insights serve as a strategic
-            compass, guiding organizations toward informed decisions,
-            sustainable growth, and competitive advantage in today's dynamic
-            business environment.
-          </p>
-        </main>
+      <section className="prose">
+        <div
+          ref={rdContent}
+          id="rd_content"
+          dangerouslySetInnerHTML={{ __html: rd.rd || "" }}
+        ></div>
+        {/* {chartData?.regional_market_share && (
+          <RegionData regions={rd?.chart?.regional_market_share || {}} />
+        )} */}
       </section>
-      <section className="mb-8">
+      {Chart && chartData && chartData.market_growth && (
+        <div
+          className="relative w-fit mx-auto border my-3 shadow-lg py-4 px-12"
+          ref={growthRef}
+          id="chart_content"
+        >
+          <div className="font-semibold text-gray-500 text-center">
+            Market Growth
+          </div>
+          <div className="flex justify-center">
+            <Chart
+              options={{
+                chart: {
+                  id: "basic-bar",
+                },
+                xaxis: {
+                  categories: Object.keys(chartData.market_growth),
+                },
+              }}
+              series={[
+                {
+                  name: "CAGR",
+                  data: Object.values(chartData.market_growth),
+                },
+              ]}
+              type="bar"
+              width={600}
+            />
+          </div>
+          <div className="text-center">www.amr.com</div>
+        </div>
+      )}
+      {Chart && chartData && chartData.market_size && (
+        <div
+          className="relative w-fit mx-auto border my-3 shadow-lg py-4 px-12"
+          ref={sizeRef}
+          id="chart_content"
+        >
+          <div className="font-semibold text-gray-500 text-center">
+            Market Size
+          </div>
+          <div className="flex justify-center">
+            <Chart
+              options={{
+                chart: {
+                  id: "basic-bar",
+                },
+                xaxis: {
+                  categories: Object.keys(chartData.market_size),
+                },
+              }}
+              series={[
+                {
+                  name: "CAGR",
+                  data: Object.values(chartData.market_size),
+                },
+              ]}
+              type="bar"
+              width={600}
+            />
+          </div>
+          <div className="text-center">www.amr.com</div>
+        </div>
+      )}
+      <section>
         <header className="my-4">
           <h3 className="text-xl font-bold uppercase border-b-4 text-primary border-secondary w-fit">
-            U.S LIFE SCIENCE ANALYTICS MARKET GROWTH FACTORS
+            DIGITAL HEALTH MARKET REPORT HIGHLIGHTS
           </h3>
         </header>
-        <main>
-          <p>
-            The U.S. life science analytics market has experienced significant
-            growth in recent years, driven by several key factors. One of the
-            primary drivers is the increasing demand for advanced analytics
-            solutions within the life sciences industry to improve operational
-            efficiency, enhance research and development processes, and optimize
-            decision-making. Technological advancements, such as big data
-            analytics, machine learning, and artificial intelli-gence, have
-            revolutionized the way data is collected, analyzed, and utilized in
-            life sciences, leading to a surge in adoption of analytics
-            solutions. Moreover, the growing emphasis on personalized medicine
-            and precision healthcare has fueled the need for sophisticated
-            analytics tools that can analyze vast amounts of patient data to
-            tailor treat-ments and therapies to individual needs, thus driving
-            market growth. Additionally, stringent regulatory require-ments and
-            compliance standards in the healthcare sector have propelled the
-            adoption of analytics solutions for ensuring regulatory compliance,
-            drug safety, and risk management. Furthermore, the COVID-19 pandemic
-            has further accelerated the uptake of analytics solutions in the
-            life sciences industry, as organizations seek to harness data-driven
-            insights to address challenges related to vaccine developmentsupply
-            chain disruptions, and epidemiological modeling. With the continuous
-            evolution of technology and the incre-asing integration of analytics
-            into various aspects of the life sciences value chain, the U.S. life
-            science analytics market is poised for robust growth in the
-            foreseeable future.The U.S. life science analytics market is
-            witnessing dynamic trends, propelled by advancements in technology,
-            increasing data complexity, and the growing demand for personalized
-            medicine and improved patient outcomes.One significant trend is the
-            integration of artificial intelligence (AI) and machine learning
-            (ML) algorithms into analytics solutions, enablingpharmaceutical
-            companies, biotech firms, and healthcare providers to derive
-            actionabl insights from vast datasets efficiently. Another emerging
-            trend is the emphasis on real-world evidence (RWE) analytics,
-            leveraging data from electronic health records (EHRs), claims
-            databases, and wearable devices to inform clinical decision-making,
-            regulatory approvals, and health policy formulation.Another emerging
-            trend is the emphasis on real-world evidence (RWE) analytics,
-            leveraging data from electronic health records (EHRs), claims
-            databases, and wearable devices to inform clinical decision-making,
-            regulatory approvals, and health policy formulation.
-          </p>
-          <div className="flex justify-center mt-8">
-            <Image src={barChart} alt="barchart" />
-          </div>
-        </main>
-      </section>
-      <section className="mb-8">
-        <header className="my-4">
-          <h3 className="text-xl font-bold uppercase border-b-4 text-primary border-secondary w-fit">
-            U.S LIFE SCIENCE ANALYTICS MARKET TRENDS
-          </h3>
-        </header>
-        <main>
-          <p>
-            The U.S. life science analytics market is witnessing dynamic trends,
-            propelled by advancements in technology, increasing data complexity,
-            and the growing demand for personalized medicine and improved
-            patient outcomes. One significant trend is the integration of
-            artificial intelligence (AI) and machine learning (ML) algorithms
-            into analytics solutions, enablingpharmaceutical companies, biotech
-            firms, and healthcare providers to derive actionabl insights from
-            vast datasets efficiently. These AI-driven analytics platforms
-            facilitate drug discovery, clinical trials optimization, and
-            real-time monit-oring of patient data, leading to more effective
-            treatments and therapies.Additionally, there's a noticeable shift
-            towards cloud-based analytics solutions, allowing organizations to
-            scale their analytics capabilities, enhance collaboration, and
-            reduce infrastructure costs. Another emerging trend is the emphasis
-            on real-world evidence (RWE) analytics, leveraging data from
-            electronic health records (EHRs), claims databases, and wearable
-            devices to inform clinical decision-making, regulatory approvals,
-            and health policy formulation. Moreover, the COVID-19 pandemic has
-            accelerated the adoption of telemedicine and remote patient
-            monitoring, driving the need for sophisticated analytics tools to
-            manage and analyze remote patient data securely. Furthermore,
-            regulatory agencies are increasingly focusing on ensuring data
-            integrity, privacy, and compliance, prompting life science companies
-            to invest in robust data governance and security solutions.
-          </p>
-          <div className="flex justify-center mt-8">
-            <Image src={roundChart} alt="barchart" />
-          </div>
-        </main>
+        <SummaryTabHighlight basic={basic} marketAnalysis={marketAnalysis} />
       </section>
       <section>
-        <SummaryTabHighlight />
-      </section>
-      <section>
-        <SummaryTabFAQ />
+        <SummaryTabFAQ
+          report={basic}
+          marketAnalysis={marketAnalysis}
+          marketReport={marketReport}
+        />
       </section>
     </div>
   );
