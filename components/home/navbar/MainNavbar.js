1ce221f3c11e6471206f1cde0fdc09c3df903bf4
Rubayet-billah <rubayetbillah909@gmail.com>
2024-04-01 00:37:26 +0600

    responsive navbar fixed

--- components/home/navbar/MainNavbar.js
+++ components/home/navbar/MainNavbar.js
@@ -50,7 +50,10 @@ const MainNavbar = ({ parentCategories = [] }) => {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content mt-3 z-[2] p-2 shadow bg-base-100 rounded w-52"
               >
-                <LinkTags pathname={pathname} />
+                <LinkTags
+                  pathname={pathname}
+                  parentCategories={parentCategories}
+                />
               </ul>
             </div>
             <Link href="/">
