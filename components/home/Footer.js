f254e2463b20e764818fd00d18145babfbaf7d31
Rubayet-billah <rubayetbillah909@gmail.com>
2024-04-02 00:32:38 +0600

    footer redesigned

--- components/home/Footer.js
+++ components/home/Footer.js
@@ -1,11 +1,9 @@
 import React from "react";
-import TopFooter from "./footer/TopFooter";
 import MainFooter from "./footer/MainFooter";
 
 const Footer = () => {
   return (
     <div>
-      <TopFooter />
       <MainFooter />
     </div>
   );
