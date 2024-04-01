7af5372d5db29ec83b26d38d098b13bbba78db41
Rubayet-billah <rubayetbillah909@gmail.com>
2024-04-01 23:57:32 +0600

    about us page footer added

--- app/about/page.js
+++ app/about/page.js
@@ -1,10 +1,12 @@
 import AboutPage from "@/components/about/AboutPage";
+import Footer from "@/components/home/Footer";
 import React from "react";
 
 const About = () => {
   return (
     <div>
       <AboutPage />
+      <Footer />
     </div>
   );
 };
