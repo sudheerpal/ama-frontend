79e44a1b833e51628d8866668715a4a1f8fecf32
Rubayet-billah <rubayetbillah909@gmail.com>
2024-04-05 20:10:58 +0600

    desktop navbar component separated

---
+++ components/home/navbar/LinkTagsDesktop.js
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import { ChevronDown } from "react-feather";
+import Link from "next/link";
+import LucidIcon from "@/components/ui/icons/LucidIcons";
+import { usePathname } from "next/navigation";
+import { links } from "@/constants/constants";
+
+const LinkTagsDesktop = ({ parentCategories }) => {
+  const pathname = usePathname();
+  return (
+    <ul className="px-4 font-medium lg:w-full lg:max-w-3xl xl:text-base menu menu-horizontal lg:justify-between">
+      {links?.map((link, index) => (
+        <li
+          key={index}
+          className={`px-4 text-primary cursor-pointer ${
+            link.submenus ? "group relative dropdown" : ""
+          }`}
+        >
+          <Link
+            className={`p-0 hover:text-blue-500 ${
+              pathname == link.url ? "text-blue-500" : ""
+            }`}
+            href={link.url}
+          >
+            {link.text} {link.submenus && <ChevronDown size={16} />}
+          </Link>
+          {/* Render submenus if they exist */}
+          {link.submenus && (
+            <div className="absolute hidden h-auto group-hover:block dropdown-menu">
+              <ul className="top-0 z-10 grid grid-cols-2 gap-2 p-4 bg-white rounded shadow w-[576px]">
+                {parentCategories?.map((submenu) => (
+                  <li key={submenu.id} className="py-1">
+                    <Link
+                      href={`/industries/${submenu.link}`}
+                      className="inline-flex items-center p-0 cursor-pointer text-primary hover:text-blue-500"
+                    >
+                      <LucidIcon name={submenu.icon} size={20} />
+                      <span> {submenu.label}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default LinkTagsDesktop;
