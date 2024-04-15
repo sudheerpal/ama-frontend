e6ad5a68051c213f74bc6a9ed34ec18fd03a6645
basir5101 <basir.bsmrstu@gmail.com>
2024-04-16 05:43:18 +0600

    seo meta tag added for report detail page

--- app/reports/[slug]/page.js
+++ app/reports/[slug]/page.js
@@ -1,8 +1,27 @@
+import SEO from "@/components/common/SEO";
 import Footer from "@/components/home/Footer";
 import Header from "@/components/home/Header";
 import ReportPage from "@/components/report/ReportPage";
 import CustomContainer from "@/components/ui/CustomContainer";
+import Head from "next/head";
 import React from "react";
+
+export const generateMetadata = async ({ params }) => {
+  let reportData = null;
+  const arrays = params.slug.split("-");
+  const reportId = arrays[arrays.length - 1];
+  const res = await fetch(`${process.env.API_URL}/api/reports/${reportId}`, {
+    cache: "no-cache",
+  });
+  const dataReport = await res.json();
+  reportData = dataReport.data;
+  const { title, summery = "", marketKeyword = "" } = reportData?.basic;
+  return {
+    title: reportData?.basic?.seo?.metaTitle || title,
+    description: reportData?.basic?.seo?.metaDescription || summery,
+    keywords: reportData?.basic?.seo?.keywords || marketKeyword,
+  };
+};
 
 const ReportDetails = async ({ params }) => {
   const arrays = params.slug.split("-");
@@ -31,7 +50,7 @@ const ReportDetails = async ({ params }) => {
   }
 
   return (
-    <>
+    <div>
       {reportData.basic ? (
         <ReportPage
           testimonials={testimonials}
@@ -44,7 +63,7 @@ const ReportDetails = async ({ params }) => {
         </div>
       )}
       <Footer />
-    </>
+    </div>
   );
 };
 
---
+++ components/common/SEO.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+
+export default function SEO({ title = "", description = "", keywords = "" }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      {/* <meta name="abstract" content={abstract} /> */}
+
+      {/* <link rel="canonical" href={canonicalUrl} />
+      <link rel="shortlink" href={canonicalUrl} /> */}
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={"marketresearchforecast"} />
+      <meta property="og:type" content="article" />
+      <meta property="og:url" content={"https://marketresearchforecast.com"} />
+
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {/* <meta name="twitter:url" content={canonicalUrl} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content={DefaultSeo.twitterSite} />
+      <meta name="twitter:image" content={imageUrl} /> */}
+
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="MobileOptimized" content="width" />
+      {/* <meta name="HandheldFriendly" content="true" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="news_keywords" content={keywords} />
+      <meta name="rights" content="Planndesign" />
+      <link rel="image_src" href={imageUrl} /> */}
+    </Head>
+  );
+}
