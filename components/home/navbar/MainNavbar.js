31b10081196add7a950ed670cc1f9a7ba5b018c0
Rubayet-billah <rubayetbillah909@gmail.com>
2024-04-24 20:42:14 +0600

    navbar toggled for mobile device

--- components/home/navbar/MainNavbar.js
+++ components/home/navbar/MainNavbar.js
@@ -1,16 +1,9 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import logo from "@/assets/logo4.jpeg";
 import Image from "next/image";
-import {
-  ChevronDown,
-  Facebook,
-  Home,
-  Instagram,
-  Linkedin,
-  Twitter,
-} from "react-feather";
+import { Facebook, Linkedin, Menu, Twitter, X } from "react-feather";
 import LinkTags from "./LinkTags";
-import { headers } from "next/headers";
 import Link from "next/link";
 import { links } from "@/constants/constants";
 import CustomContainer from "@/components/ui/CustomContainer";
@@ -18,43 +11,30 @@ import LucidIcon from "@/components/ui/icons/LucidIcons";
 import LinkTagsDesktop from "./LinkTagsDesktop";
 
 const MainNavbar = ({ parentCategories = [] }) => {
-  const heads = headers();
-  const pathname = heads.get("next-url");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
       <CustomContainer>
         <div className="p-0 navbar text-primary lg:py-4 lg:gap-5">
           <div className="navbar-start lg:max-w-sm lg:w-fit">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
+            <div className="relative">
+              <button
                 className="p-0 mr-4 btn btn-ghost lg:hidden"
+                onClick={toggleNavbar}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </div>
+                {!isOpen ? <Menu size={24} /> : <X size={24} />}
+              </button>
               <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[2] p-2 shadow bg-base-100 rounded w-72"
+                className={`menu menu-sm dropdown-content mt-3 z-[2] p-2 shadow absolute bg-white rounded w-72 ${
+                  isOpen ? "block" : "hidden"
+                }`}
               >
-                <LinkTags
-                  pathname={pathname}
-                  parentCategories={parentCategories}
-                />
+                <LinkTags parentCategories={parentCategories} />
               </ul>
             </div>
             <Link href="/">
