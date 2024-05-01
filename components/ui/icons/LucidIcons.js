dba4b195ef0e97cf9eaa802a6cde3ce0620621ef
basir5101 <basir.bsmrstu@gmail.com>
2024-05-02 01:33:32 +0600

    icon added:

--- components/ui/icons/LucidIcons.js
+++ components/ui/icons/LucidIcons.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { icons } from "lucide-react";
 
-const LucidIcon = ({ name = "BrickWall", classNames = "", size }) => {
-  const DynamicLucideIcon = icons[name];
-
-  // return <DynamicLucideIcon className={classNames} size={size || 48} />;
-  return "";
+const LucidIcon = ({ name, classNames, size }) => {
+  if (name) {
+    const DynamicLucideIcon = icons[name];
+    return <DynamicLucideIcon className={classNames} size={size || 48} />;
+  } else {
+    return "";
+  }
 };
 
 export default LucidIcon;
