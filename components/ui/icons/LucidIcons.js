8ff954ec11aab2c29bb185060a2e8271213928bb
basir5101 <basir.bsmrstu@gmail.com>
2024-05-02 01:13:15 +0600

    quick fix

--- components/ui/icons/LucidIcons.js
+++ components/ui/icons/LucidIcons.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { icons } from "lucide-react";
 
-const LucidIcon = ({ name, classNames, size }) => {
+const LucidIcon = ({ name = "BrickWall", classNames = "", size }) => {
   const DynamicLucideIcon = icons[name];
 
-  return <DynamicLucideIcon className={classNames} size={size || 48} />;
+  // return <DynamicLucideIcon className={classNames} size={size || 48} />;
+  return "";
 };
 
 export default LucidIcon;
