609876d228bdf529965dba1f4a9c78a10fa5f60f
basir5101 <basir.bsmrstu@gmail.com>
2024-05-29 00:07:29 +0600

    client api

--- app/api/logo/route.js
+++ app/api/logo/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
-    const res = axios.get(`${process.env.API_URL}/api/testimonial`);
+    const res = axios.get(`${process.env.API_URL}/api/client-logo`);
     const data = (await res).data;
     return NextResponse.json(data);
   } catch (error) {}
