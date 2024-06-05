5f9e288dac41e58347732d6b788d4986ec9cea7c
Rubayet-billah <rubayetbillah909@gmail.com>
2024-06-05 18:02:13 +0600

    rd page sticky header and click animation added

--- components/report/ReportTabs.js
+++ components/report/ReportTabs.js
@@ -6,11 +6,17 @@ import TOC from "./components/TOC";
 import SummaryTabHighlight from "./components/SummaryTabHighlight";
 import MethodologyTabContent from "./components/MethodologyTabContent";
 import { useParams, useRouter } from "next/navigation";
+import { useInView } from "react-intersection-observer";
+import TabNavigationButton from "./components/TabNavigationButton";
+import CustomContainer from "../ui/CustomContainer";
+import MRFImage from "../ui/Image";
+import mainLogo from "@/assets/logo4.jpeg";
 
 const ReportTabs = ({ basic, marketAnalysis, marketReport, rd }) => {
   const router = useRouter();
   const params = useParams();
 
+  const { ref: summaryTabRef, inView } = useInView();
   // State to track the active tab
   const [activeTab, setActiveTab] = useState(1);
   const [isMobile, setIsMobile] = useState(false);
@@ -25,6 +31,7 @@ const ReportTabs = ({ basic, marketAnalysis, marketReport, rd }) => {
     {
       id: 1,
       label: "Summary",
+      isNavbarDisplay: true,
       content: (
         <SummaryTabContent
           basic={basic}
@@ -37,6 +44,7 @@ const ReportTabs = ({ basic, marketAnalysis, marketReport, rd }) => {
     {
       id: 2,
       label: isMobile ? "TOC" : "Table of Content",
+      isNavbarDisplay: true,
       content: (
         <TOC
           marketAnalysis={marketAnalysis}
@@ -48,11 +56,17 @@ const ReportTabs = ({ basic, marketAnalysis, marketReport, rd }) => {
     {
       id: 3,
       label: "Segmentation",
+      isNavbarDisplay: true,
       content: (
         <SummaryTabHighlight basic={basic} marketAnalysis={marketAnalysis} />
       ),
     },
-    { id: 4, label: "Methodology", content: <MethodologyTabContent /> },
+    {
+      id: 4,
+      label: "Methodology",
+      isNavbarDisplay: false,
+      content: <MethodologyTabContent />,
+    },
   ];
 
   useEffect(() => {
@@ -68,10 +82,46 @@ const ReportTabs = ({ basic, marketAnalysis, marketReport, rd }) => {
 
   return (
     <div className="relative">
-      <hr className="absolute w-full border-b-2 border-primary top-[18px] md:top-[34px] z-10" />
+      {!inView && (
+        <div
+          className={`fixed top-0 duration-100 left-0 lg:-left-2 bg-white shadow-lg w-[100vw] py-3 z-20 ${
+            !inView ? "block" : "hidden"
+          }`}
+        >
+          <CustomContainer classNames="lg:flex gap-5 items-center">
+            <div className="flex gap-5">
+              <MRFImage
+                classNames="max-w-28 lg:max-w-36 xl:max-w-40 md:pr-4 max-h-12"
+                src={mainLogo}
+                alt="Market Research Future Logo"
+              />
+              <h2 className="font-bold uppercase xl:text-lg text-primary">
+                {basic?.title}
+              </h2>
+            </div>
+            <div className="mt-2 tabs-xs tabs md:tabs-md md:tabs-boxed">
+              {tabs
+                ?.filter((tab) => tab.isNavbarDisplay)
+                ?.map((tab) => (
+                  <TabNavigationButton
+                    key={tab?.id}
+                    tab={tab}
+                    activeTab={activeTab}
+                    handleTabChange={handleTabChange}
+                    isFloating
+                  />
+                ))}
+            </div>
+          </CustomContainer>
+        </div>
+      )}
+      <hr
+        ref={summaryTabRef}
+        className="absolute w-full border-b-2 border-primary top-[18px] md:top-[30px] z-10"
+      />
       <div
         role={`tablist ${basic?.id}`}
-        className="overflow-x-auto tabs-xs tabs md:tabs-md md:tabs-boxed"
+        className="overflow-x-auto md:p-0 tabs-xs tabs md:tabs-md md:tabs-boxed"
         style={{
           borderRadius: "2px",
         }}
@@ -81,6 +131,7 @@ const ReportTabs = ({ basic, marketAnalysis, marketReport, rd }) => {
             key={tab.id}
             activeTab={activeTab}
             handleTabChange={handleTabChange}
+            inView={inView}
             tab={tab}
           />
         ))}
